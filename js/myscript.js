f27f0796cb65337cea2b64d242847d77ba0d8cec
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-02-22 16:56:09 +0100

    start the resolution...

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,9 @@
+
+/* DATI
+0.21€/Km tariffa base biglietto treno
+-20% sconto under 18 anni
+-40% sconto over 65
+? = Calcola il costo del biglietto */
+
 const myDistance = prompt("How many kilometers do you have to travel?");
 const myAge = prompt("How old are you");
-
-
