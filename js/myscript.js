51f675d8faaabddb817060da22045cc91bf065bf
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-02-22 17:09:59 +0100

    first try of the program

--- js/myscript.js
+++ js/myscript.js
@@ -5,5 +5,21 @@
 -40% sconto over 65
 ? = Calcola il costo del biglietto */
 
-const myDistance = prompt("How many kilometers do you have to travel?");
-const myAge = prompt("How old are you");
+let myDistance = prompt("How many kilometers do you have to travel?");
+let myAge = prompt("How old are you");
+
+myDistance = Number(myDistance);
+myAge = Number(myAge);
+
+
+// Calculate the cost based on the distance and rate
+let cost = myDistance * 0.21;
+
+if (Myage < 18) {
+    cost *= 0.8; // Apply a 20% discount for users under 18
+  } else if (age >= 65) {
+    cost *= 0.6; // Apply a 40% discount for users 65 and over
+}
+
+// Display the cost to the user
+console.log("The cost for the ticket is" + cost + " €.");
